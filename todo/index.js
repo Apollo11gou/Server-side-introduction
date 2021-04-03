5d87a4700c6671ce40c7977d46971326f6bffd4f
Apollo11gou <next.soharuna@gmail.com>
2021-04-04 08:19:28 +0900

    todoモジュールの作成

---
+++ todo/index.js
@@ -0,0 +1,23 @@
+'use strict';
+// {name:タスクの文字列, state: 完了しているかどうかの真偽値}
+const tasks =[];
+/**
+ * TODOを追加する
+ * @param{string} task
+ */
+function add(task){
+  tasks.push({name:task, state: false});
+}
+
+/**
+ * TODOの一覧の配列を取得する
+ * @return {array}
+ */
+function list(){
+  return tasks
+  // タスクが完了していれば false を、TODO ならば true を返す
+  .filter(task => !task.state)
+  // 引数 t で配列の要素を取得して、選別されたタスクの name プロパティから文字列を取得し、その文字列だけの値に変換する
+  .map(t => t.name);
+}
+module.exports = {add, list};
