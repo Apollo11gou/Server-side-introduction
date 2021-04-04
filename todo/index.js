aacfcbd24a8ecdb8529a54db4bab98d9a415a5a3
Apollo11gou <next.soharuna@gmail.com>
2021-04-04 10:39:00 +0900

    todoアプリの作成

--- todo/index.js
+++ todo/index.js
@@ -1,20 +1,29 @@
-'use strict';
+"use strict";
 // {name:タスクの文字列, state: 完了しているかどうかの真偽値}
-const tasks =[];
+const tasks = [];
 /**
  * TODOを追加する
  * @param{string} task
  */
-function add(task){
-  tasks.push({name:task, state: false});
+function add(task) {
+  tasks.push({ name: task, state: false });
 }
 
 /**
  * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了したかを返す
  * @param {object} taskAndIsDonePair
- * @return {boolean} 完了してないかどうか
+ * @return {boolean} 完了したかどうか
  */
-function isNotDone(taskAndIsDonePair){
+function isDone(taskAndIsDonePair) {
+  return taskAndIsDonePair.state;
+}
+
+/**
+ * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了していないかを返す
+ * @param {object} taskAndIsDonePair
+ * @return {boolean} 完了していないかどうか
+ */
+function isNotDone(taskAndIsDonePair) {
   return !isDone(taskAndIsDonePair);
 }
 
@@ -22,11 +31,45 @@ function isNotDone(taskAndIsDonePair){
  * TODOの一覧の配列を取得する
  * @return {array}
  */
-function list(){
-  return tasks
+function list() {
   // タスクが完了していれば false を、TODO ならば true を返す
-  .filter(task =>isNotDone(task))
   // 引数 t で配列の要素を取得して、選別されたタスクの name プロパティから文字列を取得し、その文字列だけの値に変換する
-  .map(t => t.name);
+  return tasks.filter(isNotDone).map((t) => t.name);
 }
-module.exports = {add, list};
+
+/**
+ * TODOを完了状態にする
+ * @param {string} task
+ */
+function done(task) {
+  const indexFound = tasks.findIndex((t) => t.name === task);
+  if (indexFound !== -1) {
+    tasks[indexFound].state = true;
+  }
+}
+/**
+ * 完了済みのタスクの一覧の配列を取得する
+ * @return {array}
+ */
+function donelist() {
+  return tasks.filter(isDone).map((t) => t.name);
+}
+
+/**
+ * 項目を削除する
+ * @param{string} task
+ */
+function del(task) {
+  // ask が見つかった場合にはそのインデックスが、見つからなかった場合には -1 が indexFound に代入
+  const indexFound = tasks.findIndex((t) => t.name === task);
+  if (indexFound !== -1) {
+    tasks.splice(indexFound, 1);
+  }
+}
+module.exports = {
+  add,
+  list,
+  done,
+  donelist,
+  del,
+};
