018790af3add43f78478f835f5ffe1f904b1ed8b
Apollo11gou <next.soharuna@gmail.com>
2021-04-05 10:52:23 +0900

    promiseチェインを利用して書き換え

--- promise-test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const waitPromise = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(), 1000);
-});
-
-waitPromise.then(() => console.log("hoge"));
-console.log("fuga");
