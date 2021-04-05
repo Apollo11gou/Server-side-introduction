636be435ed7a3f162138344330277f9e196939a7
Apollo11gou <next.soharuna@gmail.com>
2021-04-05 11:08:53 +0900

    async/awaitを用いて非同期化

--- async-io-problem/app.js
+++ async-io-problem/app.js
@@ -1,11 +1,21 @@
 "use strict";
 const fs = require("fs");
 const fileName = "./test.txt";
-for (let count = 0; count < 500; count++) {
-  fs.appendFileSync(fileName, "あ", "utf8");
-  fs.appendFileSync(fileName, "い", "utf8");
-  fs.appendFileSync(fileName, "う", "utf8");
-  fs.appendFileSync(fileName, "え", "utf8");
-  fs.appendFileSync(fileName, "お", "utf8");
-  fs.appendFileSync(fileName, "\n", "utf8");
+
+function appendFilePromise(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, "utf8", () => resolve());
+  });
 }
+async function main() {
+  for (let count = 0; count < 500; count++) {
+    fs.appendFileSync(fileName, "あ", "utf8");
+    fs.appendFileSync(fileName, "い", "utf8");
+    fs.appendFileSync(fileName, "う", "utf8");
+    fs.appendFileSync(fileName, "え", "utf8");
+    fs.appendFileSync(fileName, "お", "utf8");
+    fs.appendFileSync(fileName, "\n", "utf8");
+  }
+}
+
+main();
